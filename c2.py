c790b62299bbc952994637eb72618bf7e44069d8
USER90 <132061406+PH0111@users.noreply.github.com>
2025-09-14 20:22:41 +0100

    Update client2.py

--- c2.py
+++ c2.py
@@ -14,12 +14,6 @@ try:
 
     def send(msg):
         message = msg.encode(FORMAT)
-        #msg_length = len(message)
-        #send_length = str(msg_length).encode(FORMAT)
-        #send_length += b' ' * (HEADER - len(send_length))
-        #client.send(send_length)
-        #client.send(message)
-
         client.sendall(message)
         print(client.recv(2048).decode(FORMAT))
 
